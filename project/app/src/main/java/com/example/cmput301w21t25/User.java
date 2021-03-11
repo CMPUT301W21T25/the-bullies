f7a9c91a2badefaa451be2b30e1a6ae1059d7ebe
curtiskennedy <curtisdeankennedy@gmail.com>
2021-03-11 14:55:36 -0700

    Added User and UserManager classes w/ attributes & method names

---
+++ project/app/src/main/java/com/example/cmput301w21t25/User.java
@@ -0,0 +1,41 @@
+package com.example.cmput301w21t25;
+
+
+import java.util.ArrayList;
+
+public class User {
+    private ArrayList<Experiment> subscribedExperiments;
+    private ArrayList<Trial> conductedTrials;
+    private ArrayList<Experiment> ownedExperiments;
+    //public UserProfile profile;
+
+    public User() {
+    }
+
+    private void subscribeTo(Experiment experiment) {
+    }
+
+    private void unsubscribeFrom(Experiment experiment) {
+    }
+
+    private void conductTrial() {
+    }
+
+    private void uploadTrial() {
+    }
+
+    private void generateQRCode() {
+    }
+
+    private void registerBarCode() {
+    }
+
+    private void scanQRCode() {
+    }
+
+    private void scanBarCode() {
+    }
+
+    private void printQRCode() {
+    }
+}
---
+++ project/app/src/main/java/com/example/cmput301w21t25/UserManager.java
@@ -0,0 +1,17 @@
+package com.example.cmput301w21t25;
+
+import java.util.ArrayList;
+
+public class UserManager {
+    private User selectedUser;
+    private ArrayList<User> userList;
+
+    public UserManager() {
+    }
+
+    public void newUser() {
+    }
+
+    public void deleteUser() {
+    }
+}
