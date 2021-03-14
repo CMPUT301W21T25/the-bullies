a66410198c4220711843f3687af9120d68d0e67d
Eden Knechtel <71038977+edenemilynan@users.noreply.github.com>
2021-03-14 17:47:15 -0600

    Update Experiment.java
    Added in automatically deleted code as comment for safety in case needed.

--- project/app/src/main/java/com/example/cmput301w21t25/Experiment.java
+++ project/app/src/main/java/com/example/cmput301w21t25/Experiment.java
@@ -2,8 +2,37 @@ package com.example.cmput301w21t25;
 
 import java.util.ArrayList;
 
-public abstract class Experiment {
+/* import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+public class Experiment { //make abstract
+
+ADDED BEFORE DUNCAN'S MERGE IN CASE NEEDED (android automatically deleted)
+*/  
+
+public abstract class Experiment {  
+    
+    
     /****************************************
                     ATTRIBUTES
     ****************************************/
