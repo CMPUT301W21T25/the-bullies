01d31a63049f645e8a1dada2889f34771ff28c4d
Samadhi <samadhi@ualberta.ca>
2021-03-15 21:22:58 -0600

    Finished Trial and created BinomialTrial

--- project/app/src/main/java/com/example/cmput301w21t25/Trial.java
+++ project/app/src/main/java/com/example/cmput301w21t25/Trial.java
@@ -1,22 +1,33 @@
 package com.example.cmput301w21t25;
 
-
-public class Trial {
-
-
-
-
+/**
+ * This is an abstract class which creates a trial based on which experiment type was used
+ * @author Samadhi
+ */
+public abstract class Trial {
 
     /****************************************
                     ATTRIBUTES
      ****************************************/
     private User user;
+    private String experimentName;
+    private User experimenter;
+    //TODO: implement geolocation info for final product!
 
     /****************************************
                 CONSTRUCTORS
      ****************************************/
-    public Trial(User user) {
+
+    /**
+     * Constructor for Trial
+     * @param user the user of the app
+     * @param experimenter the creator of the experiment
+     * @param experimentName the name of the experiment
+     */
+    public Trial(User user, User experimenter, String experimentName) {
         this.user = user;
+        this.experimenter = experimenter;
+        this.experimentName = experimentName;
     }
     public Trial() {}
 
@@ -28,4 +39,11 @@ public class Trial {
         return user;
     }
 
+    public String getExperimentName() {
+        return experimentName;
+    }
+
+    public User getExperimenter() {
+        return experimenter;
+    }
 }
