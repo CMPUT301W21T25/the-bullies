79d461997c7aa128e133ceb7dfd89fe87f5c7108
Duncan Krammer <krammer@ualberta.ca>
2021-03-15 23:32:03 -0600

    Merge branch 'sma-Layouts' of https://github.com/CMPUT301W21T25/the-boolies into sma-Layouts

---
+++ project/app/src/main/java/com/example/cmput301w21t25/CreateExperimentActivity.java
@@ -0,0 +1,14 @@
+package com.example.cmput301w21t25;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateExperimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_experiment);
+    }
+}
