beff9ca06c4d95ce4315f79148e61982045f62c0
curtiskennedy <curtisdeankennedy@gmail.com>
2021-03-16 17:45:02 -0600

    setting up connections for user view

--- project/app/src/main/java/com/example/cmput301w21t25/InspectExperimentActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/InspectExperimentActivity.java
@@ -15,12 +15,19 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class InspectExperimentActivity extends AppCompatActivity {
+
+    private String expID;
+    private String ownerID;
+    private String currentUserID;
+
     @Override
     protected void onCreate(Bundle passedData) {
         super.onCreate(passedData);
         setContentView(R.layout.activity_home_subbed);
         String experimentID;
         experimentID = getIntent().getStringExtra("EXP_ID");
+        expID = experimentID;
+        currentUserID = getIntent().getStringExtra("")
         FB_FetchExperiment(experimentID);
         finish();
     }
@@ -64,6 +71,7 @@ public class InspectExperimentActivity extends AppCompatActivity {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         String userID = (String)document.getData().get("owner");
+                        ownerID = userID;
                         DocumentReference docRef = db.collection("UserProfile").document(userID);
                         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                             @Override
@@ -101,10 +109,17 @@ public class InspectExperimentActivity extends AppCompatActivity {
      */
     public void viewExpOwnerButton(View view) {
         //first we need to get the experiment owner
+        FB_FetchOwnerProfile(expID);
 
         //check if current user = experiment owner
+        if (this.ownerID == this.currentUserID) {
+            //switch to myprofile, pass info
+            Intent intent =
+        }
+        else {
+            //switch to otherprofile, pass info
+        }
 
-        //switch to myprofile or otherprofile view depending on previous comment
     }
 
 }
