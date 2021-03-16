026dc3c716cc46c9e39ccb6fe8593e25a92c908e
Samadhi <samadhi@ualberta.ca>
2021-03-16 16:02:50 -0600

    Cleaned xml files, added view_exp xml

--- project/app/src/main/java/com/example/cmput301w21t25/MainActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         //testing DB ill make proper tests later i promise -YA
         //tests for UserMangaer class and UserProfile collection
 //        testM = new UserManager();
