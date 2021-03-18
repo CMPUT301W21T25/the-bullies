7d3052706552b60ba84b328de1ce36b5f249aa0c
Eden <eknechte@ualberta.ca>
2021-03-17 23:15:13 -0600

    Layout for measurement trial and skeleton code for activity

---
+++ project/app/src/main/java/com/example/cmput301w21t25/ConductMeasurementTrialActivity.java
@@ -0,0 +1,46 @@
+package com.example.cmput301w21t25;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConductMeasurementTrialActivity extends AppCompatActivity {
+
+    Toolbar trialHeader;
+    Button submitTrialButton;
+    EditText measurementDisplay;
+
+    private String userID;
+    private String measurementString;
+    private Float measurement;
+
+    @Override
+    protected void onCreate(@Nullable Bundle passedData) {
+        super.onCreate(passedData);
+        setContentView(R.layout.activity_conduct_count_trial);
+
+        //Grab user ID
+        userID = getIntent().getStringExtra("USER_ID");
+
+        //Need to pass experiment ID to access title, description, etc. to pass to toolbar
+
+        trialHeader = findViewById(R.id.measurementExperimentInfo);
+        submitTrialButton = findViewById(R.id.submit_trial_measurement_button);
+        measurementDisplay = findViewById(R.id.measurementEntry);
+
+        //On click, confirm trial, return to trial list view
+        submitTrialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent return to list view and add to trial list
+                measurementString = measurementDisplay.getText().toString();
+                measurement = Float.parseFloat(measurementString);
+            }
+        });
+    }
+}
