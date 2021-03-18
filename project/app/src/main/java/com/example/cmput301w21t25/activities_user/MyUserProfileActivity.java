4f8e4cef49852f50fdc048c0c19e9bbd3c88c03f
curtiskennedy <curtisdeankennedy@gmail.com>
2021-03-18 17:20:40 -0600

    MyUserView update and back functionality

--- project/app/src/main/java/com/example/cmput301w21t25/activities_user/MyUserProfileActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/activities_user/MyUserProfileActivity.java
@@ -1,31 +1,40 @@
 package com.example.cmput301w21t25.activities_user;
 
+import android.content.Intent;
 import android.os.Bundle;
+//import android.os.UserManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cmput301w21t25.R;
+import com.example.cmput301w21t25.activities_main.HomeOwnedActivity;
+import com.example.cmput301w21t25.activities_main.HomeSubbedActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.example.cmput301w21t25.managers.UserManager;
+
 
 public class MyUserProfileActivity extends AppCompatActivity {
     //attributes
     private String Username;
     private String ContactInfo;
     private String userID;
+    private String prevScreen;
 
     @Override
     protected void onCreate(Bundle passedData) {
         super.onCreate(passedData);
         setContentView(R.layout.activity_myprofile_view);
         userID = getIntent().getStringExtra("userID");
+        prevScreen = getIntent().getStringExtra("prevScreen");
         FB_FetchUserInfo(userID);
     }
 
@@ -64,5 +73,35 @@ public class MyUserProfileActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void goBackButton(View view) {
+        Intent intent = null;
+        switch (prevScreen) {
+            case "Owned":
+                intent = new Intent(MyUserProfileActivity.this, HomeOwnedActivity.class);
+                break;
+            case "Subbed":
+                intent = new Intent(MyUserProfileActivity.this, HomeSubbedActivity.class);
+                break;
+            case "Experiment":
+                // go back to experiment view
+            //default:
+                //intent = new Intent(MyUserProfileActivity.this, HomeOwnedActivity.class);
+        }
+
+        intent.putExtra("USER_ID", userID);
+        startActivity(intent);
+    }
+    public void updateButton(View view) {
+        EditText newName = findViewById(R.id.updateName);
+        EditText newEmail = findViewById(R.id.updateEmail);
+        String name = newName.getText().toString();
+        String email = newEmail.getText().toString();
+        UserManager userManager = new UserManager();
+        userManager.FB_UpdateName(name, userID);
+        userManager.FB_UpdateEmail(email, userID);
+
+        goBackButton(view);
     }
 }
