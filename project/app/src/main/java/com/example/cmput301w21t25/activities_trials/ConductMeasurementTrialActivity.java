aba0dcbaf7cd7697c724bb3df9ddad306d38b719
Eden <eknechte@ualberta.ca>
2021-03-18 23:07:22 -0600

    Added adapter to AddTrialActivity

--- project/app/src/main/java/com/example/cmput301w21t25/activities_trials/ConductMeasurementTrialActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/activities_trials/ConductMeasurementTrialActivity.java
@@ -1,5 +1,6 @@
 package com.example.cmput301w21t25.activities_trials;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cmput301w21t25.R;
+import com.example.cmput301w21t25.experiments.Experiment;
 
 public class ConductMeasurementTrialActivity extends AppCompatActivity {
 
@@ -17,6 +19,7 @@ public class ConductMeasurementTrialActivity extends AppCompatActivity {
     Button submitTrialButton;
     EditText measurementDisplay;
 
+    private Experiment trialParent;
     private String userID;
     private String measurementString;
     private Float measurement;
@@ -28,6 +31,7 @@ public class ConductMeasurementTrialActivity extends AppCompatActivity {
 
         //Grab user ID
         userID = getIntent().getStringExtra("USER_ID");
+        trialParent = (Experiment) getIntent().getSerializableExtra("TRIAL_PARENT");
 
         //Need to pass experiment ID to access title, description, etc. to pass to toolbar
 
@@ -39,9 +43,13 @@ public class ConductMeasurementTrialActivity extends AppCompatActivity {
         submitTrialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent return to list view and add to trial list
                 measurementString = measurementDisplay.getText().toString();
                 measurement = Float.parseFloat(measurementString);
+                //Intent return to list view and add to trial list
+                Intent switchScreen = new Intent(ConductMeasurementTrialActivity.this, AddTrialActivity.class);
+                switchScreen.putExtra("USER_ID", userID);
+                switchScreen.putExtra("TRIAL_PARENT", trialParent);
+                startActivity(switchScreen);
             }
         });
     }
