d7844477fa1c6f0858ceb9f33315aca5de5c1b82
curtiskennedy <curtisdeankennedy@gmail.com>
2021-03-19 00:04:05 -0600

    browse list is clickable and view profile-able

--- project/app/src/main/java/com/example/cmput301w21t25/activities_main/SearchActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/activities_main/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.cmput301w21t25.activities_main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cmput301w21t25.FilterSearchFragment;
 import com.example.cmput301w21t25.R;
+import com.example.cmput301w21t25.activities_experiments.ViewExperimentActivity;
+import com.example.cmput301w21t25.activities_user.MyUserProfileActivity;
 import com.example.cmput301w21t25.adapters.CustomListExperiment;
 import com.example.cmput301w21t25.experiments.BinomialExperiment;
 import com.example.cmput301w21t25.experiments.CountExperiment;
@@ -48,6 +51,26 @@ public class SearchActivity extends AppCompatActivity {
         browseList = findViewById(R.id.search_exp_list_view);
         experimentArrayAdapter = new CustomListExperiment(this, experimentList);
         browseList.setAdapter(experimentArrayAdapter);
+
+        browseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("DK: ", "Position clicked = " + position);
+                Experiment experiment = (Experiment) browseList.getItemAtPosition(position);
+                Intent viewExp = new Intent(SearchActivity.this, ViewExperimentActivity.class);
+
+                Bundle expBundle = new Bundle();
+                expBundle.putSerializable("EXP_OBJ", experiment);
+
+                viewExp.putExtra("USER_ID", userID);
+                viewExp.putExtra("EXP_BUNDLE", expBundle);
+
+                startActivity(viewExp);
+            }
+        });
+
+
+
         searchButton = findViewById(R.id.exp_filter_button);
 
         FB_FetchExperimentList(userID);
@@ -159,6 +182,18 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
-
+    /**
+     * Is called when a user clicks on their profile image
+     * Will switch to a profile view activity
+     * Curtis
+     * @param view
+     */
+    public void viewBrowseiButton(View view) {
+        //switch to profileView, pass userId
+        Intent intent = new Intent(SearchActivity.this, MyUserProfileActivity.class);
+        intent.putExtra("userID", userID);
+        intent.putExtra("prevScreen", "Browse");
+        startActivity(intent);
+    }
 
 }
