2f12897f760098e08006317030cc9cd7081e452b
Samadhi <samadhi@ualberta.ca>
2021-04-01 21:54:09 -0600

    Menu buttons colour coordinated!

--- project/app/src/main/java/com/example/cmput301w21t25/activities_main/SearchActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/activities_main/SearchActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -110,7 +111,7 @@ public class SearchActivity extends AppCompatActivity implements FilterSearchFra
         super.onCreateOptionsMenu(menu);
 
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.toolbar_menu,menu);
+        menuInflater.inflate(R.menu.toolbar_menu_blue,menu);
         return true;
     }
 
