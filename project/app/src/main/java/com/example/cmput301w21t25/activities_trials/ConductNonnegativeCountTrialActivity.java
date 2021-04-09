49b064cc9fef814c4aa77e80182d17e04ae524ac
curtiskennedy <curtisdeankennedy@gmail.com>
2021-04-08 23:02:45 -0600

    nonNegative trial geoloc bugfix

--- project/app/src/main/java/com/example/cmput301w21t25/activities_trials/ConductNonnegativeCountTrialActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/activities_trials/ConductNonnegativeCountTrialActivity.java
@@ -118,7 +118,7 @@ public class ConductNonnegativeCountTrialActivity extends AppCompatActivity {
         //Location;
         locationClient = LocationServices.getFusedLocationProviderClient(this);
         TrialLocationCheck(); // Check if Experiment requires location
-        Button setButton = (Button) findViewById(R.id.getLocButton);
+        Button setButton = (Button) findViewById(R.id.button3);
         setButton.setVisibility(View.GONE);
         maps = new Maps();
         setButton.setOnClickListener(new View.OnClickListener() {
