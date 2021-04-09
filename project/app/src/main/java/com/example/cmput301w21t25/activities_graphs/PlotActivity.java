5868781ff4ca134a332d739604eeff5f7ca4ca26
Eden <eknechte@ualberta.ca>
2021-04-08 21:38:26 -0600

    Plot point calcs for count trials

--- project/app/src/main/java/com/example/cmput301w21t25/activities_graphs/PlotActivity.java
+++ project/app/src/main/java/com/example/cmput301w21t25/activities_graphs/PlotActivity.java
@@ -9,6 +9,7 @@ import com.example.cmput301w21t25.managers.SummaryCalculator;
 import com.example.cmput301w21t25.trials.MeasurableTrial;
 import com.example.cmput301w21t25.trials.Trial;
 import com.github.mikephil.charting.data.Entry;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -23,11 +24,11 @@ public class PlotActivity extends AppCompatActivity {
 
         ArrayList<Float> upToDate = new ArrayList<>();
         ArrayList<Entry> plotValues = new ArrayList<>();
-        trials.sort(Comparator.comparing(trial -> trial.getDate()));
+        trials.sort(Comparator.comparing(Trial::getDate));
 
         Date currentDate = trials.get(0).getDate();
-        Integer currentPosition = 0;
-        Integer dataPoint = 0;
+        int currentPosition = 0;
+        int dataPoint = 0;
 
         MeasurableTrial trial = (MeasurableTrial) trials.get(currentPosition);
 
@@ -43,6 +44,60 @@ public class PlotActivity extends AppCompatActivity {
         }
 
     return plotValues;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public ArrayList<Entry> countByDay(ArrayList<Trial> trials) {
+
+        int upToDate = 0;
+        ArrayList<Entry> plotValues = new ArrayList<>();
+        trials.sort(Comparator.comparing(Trial::getDate));
+
+        Date currentDate = trials.get(0).getDate();
+        int currentPosition = 0;
+        int dataPoint = 0;
+
+        MeasurableTrial trial = (MeasurableTrial) trials.get(currentPosition);
+
+        while (currentPosition < trials.size()) {
+            while (trial.getDate().getDate() == currentDate.getDate() && trial.getDate().getMonth() == currentDate.getMonth() && trial.getDate().getYear() == currentDate.getYear()) {
+                upToDate += trial.getResult();
+                currentPosition += 1;
+                trial = (MeasurableTrial) trials.get(currentPosition);
+            }
+            plotValues.add(new Entry(dataPoint, upToDate));
+            dataPoint += 1;
+            currentDate = trials.get(currentPosition + 1).getDate();
+        }
+
+        return plotValues;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public ArrayList<Entry> successRateByDay(ArrayList<Trial> trials) {
+
+        ArrayList<Float> upToDate = new ArrayList<>();
+        ArrayList<Entry> plotValues = new ArrayList<>();
+        trials.sort(Comparator.comparing(Trial::getDate));
+
+        Date currentDate = trials.get(0).getDate();
+        int currentPosition = 0;
+        int dataPoint = 0;
+
+        MeasurableTrial trial = (MeasurableTrial) trials.get(currentPosition);
+
+        while (currentPosition < trials.size()) {
+            while (trial.getDate().getDate() == currentDate.getDate() && trial.getDate().getMonth() == currentDate.getMonth() && trial.getDate().getYear() == currentDate.getYear()) {
+                upToDate.add(trial.getResult());
+                currentPosition += 1;
+                trial = (MeasurableTrial) trials.get(currentPosition);
+            }
+            plotValues.add(new Entry(dataPoint, calculator.calculateMean(upToDate)));
+            dataPoint += 1;
+            currentDate = trials.get(currentPosition + 1).getDate();
+        }
+
+        return plotValues;
     }
 }
 
